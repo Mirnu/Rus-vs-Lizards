fcdb6763c9331c1d5fd05633a6d5493766a35c01
Mernya <kirillprofi007@gmail.com>
2024-02-19 17:42:42 +0300

    feat: added jsonConfig

--- Assets/Scripts/Presenters/DialogPresenter.cs
+++ Assets/Scripts/Presenters/DialogPresenter.cs
@@ -2,16 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogPresenter : MonoBehaviour
+public class DialogPresenter
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    public void OptionSelected()
     {
         
     }
